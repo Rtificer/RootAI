81f46947f1b193bbe75fc66334eec96bd68c4831
Artificer <rtificr@gmail.com>
2025-06-10 16:49:20 -0400

    added more stuff to the comments.s

--- src/game.cpp
+++ src/game.cpp
@@ -10,6 +10,7 @@ struct Clearing {
             3 = Blocked
         } x 12 for each clearing
     }
+        
     24 bits: Building Slots (4 slots times 5 bits for building type, plus 1 bit for Elder Treetop Toggle) {
         Slot (6 bits): {
             Current Building (5 bits): {
@@ -42,78 +43,59 @@ struct Clearing {
             }
         } x 4 for each possible slot
     }
-    Bits 23: Tokens {
+
+    28 Bits: Tokens {
         Bits 1-3: Wood (0-8 (9-15 unused))
         Bit 4: Keep (0-1)
-        Bit 46: Sympathy (0-1)
-        Bit 47: Mouse Trade Post (0-1)
-        Bit 48: Fox Trade Post (0-1)
-        Bit 49: Rabbit Trade Post (0-1)
-        Bit 50: Bomb Plot (0-1)
-        Bit 51-52: Snare Plot (0 = none, 1 = Face Down, 2 = Face Up, 3 = unused)
-        Bits 53-54: Extortion Plot (0 = none, 1 = Face Down, 2 = Face Up, 3 = unused)
-        Bits 55-56: Raid Plot (0 = none, 1 = Face Down, 2 = Face Up, 3 = unused)
-        Bit 57: Tunnel (0-1)
-        Bit 58: Mob (0-1)
-        Bit 59 Figure Value 1 (0-1)
-        Bit 60 Figure Value 2 (0-1)
-        Bits 61-62 Figure Value 3 (0-2, 3 = unused)
-        Bit 63: Tablet Value 1 (0-1)
-        Bit 64: Tablet Value 2 (0-1)
-    } Bits 41-63: Tokens {
-        Bits 41-44: Wood (0-8 (9-15 unused))
-        Bit 45: Keep (0-1)
-        Bit 46: Sympathy (0-1)
-        Bit 47: Mouse Trade Post (0-1)
-        Bit 48: Fox Trade Post (0-1)
-        Bit 49: Rabbit Trade Post (0-1)
-        Bit 50: Bomb Plot (0-1)
-        Bit 51-52: Snare Plot (0 = none, 1 = Face Down, 2 = Face Up, 3 = unused)
-        Bits 53-54: Extortion Plot (0 = none, 1 = Face Down, 2 = Face Up, 3 = unused)
-        Bits 55-56: Raid Plot (0 = none, 1 = Face Down, 2 = Face Up, 3 = unused)
-        Bit 57: Tunnel (0-1)
-        Bit 58: Mob (0-1)
-        Bit 59 Figure Value 1 (0-1)
-        Bit 60 Figure Value 2 (0-1)
-        Bits 61-62 Figure Value 3 (0-2, 3 = unused)
-        Bit 63: Tablet Value 1 (0-1)
-        Bit 64: Tablet Value 2 (0-1)
+        Bit 5: Sympathy (0-1)
+        Bit 6: Mouse Trade Post (0-1)
+        Bit 7: Fox Trade Post (0-1)
+        Bit 8: Rabbit Trade Post (0-1)
+        Bit 9: Bomb Plot (0-1)
+        Bit 10-11: Snare Plot (0 = none, 1 = Face Down, 2 = Face Up, 3 = unused)
+        Bits 12-13: Extortion Plot (0 = none, 1 = Face Down, 2 = Face Up, 3 = unused)
+        Bits 14-15: Raid Plot (0 = none, 1 = Face Down, 2 = Face Up, 3 = unused)
+        Bit 16: Tunnel (0-1)
+        Bit 17: Mob (0-1)
+        Bit 18 Figure Value 1 (0-1)
+        Bit 19 Figure Value 2 (0-1)
+        Bits 20-21 Figure Value 3 (0-2, 3 = unused)
+        Bit 22: Tablet Value 1 (0-1)
+        Bit 23: Tablet Value 2 (0-1)
+        Bits 24-25: Tablet Value 3 (0-2, 3 = unused)
+        Bit 26: Jewelry Value 1 (0-1)
+        Bit 27: Jewelry Value 2 (0-1)
+        Bit 28: Jewelry Value 3 (0-2, 3 = unused)
     }
-    */
-    int64_t data;
 
-    /*
-    Bits 1-5: Tokens Continued {
-        Bits 1-2: Tablet Value 3 (0-2, 3 = unused)
-        Bit 3: Jewelry Value 1 (0-1)
-        Bit 4: Jewelry Value 2 (0-1)
-        Bit 5: Jewelry Value 3 (0-2, 3 = unused)
+    44 Bits Warrior Data {
+        Bits 1-5: Marquise de Cat Warriors (0-25, 26-31 uneeded)
+        Bits 6-10: Eyrie Dynasties Warriors (0-20, 21-31 uneeded)
+        Bit 11: Vagabond 1 (0-1)
+        Bit 12: Vagabond 2 (0-1)
+        Bits 13-16: Woodland Alliance Warriors (0-10, 11-15 uneeded)
+        Bits 17-20: Riverfolk Company Warriors (0-15)
+        Bits 21-25: Lizard Cult Warriors (0-25, 26-31 uneeded)
+        Bits 26-29: Corvid Conspiracy Warriors (0-15)
+        Bits 30-34: Underground Duchy Warriors (0-20, 21-31 uneeded)
+        Bits 35-39: Lord of the Hundreds Warriors (0-20, 21-31 uneeded)
+        Bit 40: Lord of the Hundreds Warlord (0-1)
+        Bits 41-44: Keepers in Iron Warriors (0-15)
     }
-    Bits 6-48 Warrior Data {
-        Bits 6-10: Marquise de Cat Warriors (0-25, 26-31 uneeded)
-        Bits 11-15: Eyrie Dynasties Warriors (0-20, 21-31 uneeded)
-        Bit 16: Vagabond 1 (0-1)
-        Bit 17: Vagabond 2 (0-1)
-        Bits 18-21: Woodland Alliance Warriors (0-10, 11-15 uneeded)
-        Bits 22-25: Riverfolk Company Warriors (0-15)
-        Bits 26-30: Lizard Cult Warriors (0-25, 26-31 uneeded)
-        Bits 31-34: Corvid Conspiracy Warriors (0-15)
-        Bits 35-39: Underground Duchy Warriors (0-20, 21-31 uneeded)
-        Bits 40-44: Lord of the Hundreds Warriors (0-20, 21-31 uneeded)
-        Bit 45: Lord of the Hundreds Warlord (0-1)
-        Bits 46-49: Keepers in Iron Warriors (0-15)
-    }
-    Bits 50-51: Clearing Type {
+
+    2 Bits: Clearing Type {
         0 = Mouse
         1 = Fox
         2 = Rabbit
         3 = None
     }
-    Bits 52-55: Connected Forests (0-8)
-    Bit 56: Razed? (0-1)
 
+    24 Bits: Connected Forests {
+        4 Bits: Clearing Index (0-12, 13-14 unused)
+    } x 6 for each possible connection
+
+    1 Bit: Razed? (0-1)
     */
-   int64_t data_continued;
 };
 
 class Game {
